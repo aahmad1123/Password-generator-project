913aea33efc66ed8d04a4fa841c2b99893e083b1
aahmad1123 <ayat.asaad@gmail.com>
2024-01-11 13:50:42 -0600

    added conditional statements for if password is < 8 or >128, added semicolons

--- script.js
+++ script.js
@@ -9,49 +9,52 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var numberArray = "1234567890".split("")
+var numberArray = "1234567890".split("");
 
-var lowerCaseArray="abcdefghijklmnopqrstuvwxyz".split("")
-var upperCaseArray="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var lowerCaseArray="abcdefghijklmnopqrstuvwxyz".split("");
+var upperCaseArray="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-var specialCharactersArray = "!@#$%^&*()".split("")
+var specialCharactersArray = "!@#$%^&*()".split("");
 
 // goal is to get randomly generated string as return of generate password function. 
 // create pools of potential characters in array
-var characterPool = []
-var finalPassword = []
+var characterPool = [];
+var finalPassword = [];
 function generatePassword(){
-var length = prompt("How long would you like your password to be?")
-if (length > 128){
-  var length = prompt("Please enter a value below 128")
+var length = prompt("How long would you like your password to be?");
+if (length < 8){
+  var length = prompt("Please enter a value of 8 or higher");
 }
-var specialCharacters = confirm("Special characters?")
-var upperCase = confirm("Uppercase?")
-var lowerCase = confirm("Lowercase?")
-var numbers = confirm("Numbers?")
+if (length > 128){
+  var length = prompt("Please enter a value below 128");
+}
+var specialCharacters = confirm("Special characters?");
+var upperCase = confirm("Uppercase?");
+var lowerCase = confirm("Lowercase?");
+var numbers = confirm("Numbers?");
 
 if (specialCharacters === true) {
-  characterPool = characterPool.concat(specialCharactersArray)
+  characterPool = characterPool.concat(specialCharactersArray);
 }
 if (upperCase === true){
-  characterPool = characterPool.concat(upperCaseArray)
+  characterPool = characterPool.concat(upperCaseArray);
 }
 if(lowerCase === true){
-  characterPool = characterPool.concat(lowerCaseArray)
+  characterPool = characterPool.concat(lowerCaseArray);
 }
 if(numbers === true){
-  characterPool = characterPool.concat(numberArray)
+  characterPool = characterPool.concat(numberArray);
 }
 console.log(characterPool)
 for (var i=0; i < length; i++){
 
   var randomIndex = Math.floor(Math.random() * characterPool.length);
-  var characterValue = characterPool[randomIndex]
-  finalPassword.push(characterValue)
-  console.log(finalPassword)
+  var characterValue = characterPool[randomIndex];
+  finalPassword.push(characterValue);
+  console.log(finalPassword);
 }
 
-  return finalPassword.join("")
+  return finalPassword.join("");
 }
 
 
